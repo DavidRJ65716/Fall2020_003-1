8ddc7e87403aae9ca18d5ce3a3f08fb883a86684
ericsananikone <ericsananikone@localhost>
2020-10-01 18:51:54 -0500

    Added a print statement to 'TravelDestination' in my branch.

--- Lab 5/src/TravelDestination.java
+++ Lab 5/src/TravelDestination.java
@@ -3,6 +3,9 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
+		
+		//Adding print statement with name and hobby
+		System.out.println("My name is Eric Sananikone. One of my hobbies is running.");
 	}
 
 }
